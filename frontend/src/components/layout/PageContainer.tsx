02759e101b073fcfc221253982579bba9621fcbf
RockyRori <luosuhairocky@gmail.com>
2025-11-16 00:58:17 +0800

    Updates

--- frontend/src/components/layout/PageContainer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface Props {
-  children: React.ReactNode;
-}
-
-export const PageContainer: React.FC<Props> = ({ children }) => {
-  return <main className="gap-page-container">{children}</main>;
-};
